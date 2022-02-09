8982829f724005af2515ea8461c2a8b1afa8c194
ThinkMuk <b2000kang@gmail.com>
2022-02-09 21:11:19 +0900

    2022-02-09 study (afternoon)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import { Button, Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import "./App.css";
 import shoeListdata from "./data.js";
+import { Link, Route, Switch } from "react-router-dom";
 
 // npm start 보다는 yarn start 가 더 빠르고 안정적으로 시작할 수 있다
 // 본인이 디자인 하기 힘들때는, BootStrap 이라는 외부 source library를 이용해서 사이트를
 // 만들 수도 있다
 // 사용하기 위해서는 react bootstrap을 검색해 따라하면 된다
+
+// react router을 사용하기 위해선 yarn add react-router-dom 이라는 라이브러리를 설치 해야 한다
+// 설치를 완료한 후, index.js에 들어가서 추가적인 설정을 해주면 react router을 사용할 준비가 끝난다
 
 function App() {
   const [shoeDatas, setShoes] = useState(shoeListdata);
@@ -50,34 +54,53 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="jumbotron">
-        <div>
-          <h1>20% Season Off</h1>
-          <p>
-            Give your feet the beauty treatment that only brand new shoes can
-            give.
-          </p>
-          <p>
-            <Button variant="dark">Learn more</Button>{" "}
-          </p>
+      {/* exact path는 그 페이지일때만 해당 html을 표시하는것 */}
+      <Route exact path="/">
+        <div className="jumbotron">
+          <div>
+            <h1>20% Season Off</h1>
+            <p>
+              Give your feet the beauty treatment that only brand new shoes can
+              give.
+            </p>
+            <p>
+              <Button variant="dark">Learn more</Button>{" "}
+            </p>
+          </div>
         </div>
-      </div>
+        {/* col-md-4 같은 경우는 bootstrap에서 지원해주는 반응형이다 */}
+        <div className="container">
+          <div className="row">
+            {shoeImages.map(function (shoeTemp, i) {
+              return (
+                <ShoeList
+                  key={i}
+                  tempCount={i}
+                  tempShoeImage={shoeImages}
+                  tempShoe={shoeDatas}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </Route>
 
-      {/* col-md-4 같은 경우는 bootstrap에서 지원해주는 반응형이다 */}
-      <div className="container">
-        <div className="row">
-          {shoeImages.map(function (shoeTemp, i) {
-            return (
-              <ShoeList
-                key={i}
-                tempCount={i}
-                tempShoeImage={shoeImages}
-                tempShoe={shoeDatas}
-              />
-            );
-          })}
+      <Route exact path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="/images/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
         </div>
-      </div>
+      </Route>
+      {/* 라우트에 컴포넌트도 넣을 수 있다! */}
     </div>
   );
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+
+// BrowserRouter 와 HashRouter이 있는데 차이점은
+// HashRouter은 더 안전하게 라우팅 할 수 있게 해줌
+// BrowserRouter 같은 경우 라우팅을 리액트가 아니라 서버에게 요청할 수도 있어서 위험
+// 따라서, BrowserRouter은 서버에서 서버 라우팅 방지하는 API를 작성해둬야함
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
