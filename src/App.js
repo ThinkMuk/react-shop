21758803df4272067b60bb82761afab565602d30
ThinkMuk <b2000kang@gmail.com>
2022-02-13 20:54:00 +0900

    2022-02-13 study

--- src/App.js
+++ src/App.js
@@ -7,42 +7,6 @@ import { Link, Route, Routes, Switch } from "react-router-dom";
 import Detail from "./components/Detail.js";
 import axios from "axios";
 
-// npm start 보다는 yarn start 가 더 빠르고 안정적으로 시작할 수 있다
-// 본인이 디자인 하기 힘들때는, BootStrap 이라는 외부 source library를 이용해서 사이트를
-// 만들 수도 있다
-// 사용하기 위해서는 react bootstrap을 검색해 따라하면 된다
-
-// react router을 사용하기 위해선 yarn add react-router-dom 이라는 라이브러리를 설치 해야 한다
-// 설치를 완료한 후, index.js에 들어가서 추가적인 설정을 해주면 react router을 사용할 준비가 끝난다
-
-// sass란 ?
-// css에서 변수, 연산자, 함수, extend, import 와 같은 function들을 사용 가능하다
-
-//Ajax?
-//서버에 '새로고침'없이 요청을 할 수 있게 도와줌
-//서버란?
-//서버는 누군가 요청을 하면 페이지를 갖다주는 프로그램
-//Ajax를 사용하는 3가지 방법
-//1.jQuery 설치해서 $.ajax() 쓰기
-//2.axios 설치해서 axios.get() 쓰기 - yarn add axios
-//3.쌩 자바스크립트 fetch() 쓰기
-//axios를 사용하면 JSON을 object로 알아서 바꿔주기 때문에 간편하다
-
-//간단한 데이터 전송은 모두 props를 사용해서 넘기지만, 가끔 복잡한 데이터 전달 형식이 나올때는 contextAPI를 사용하면
-//덜 헷갈리게 가져올 수 있는 이점이 생긴다 ex) 컴포넌트 안에.. 컴포넌트 안에... 컴포넌트 안에..
-//context 만들기:
-//  1. React.createContext()로 범위 생성하기 (context는 여러개 만들기도 가능)
-//  2. 같은 값을 공유할 HTML을 <범위>로 싸매기
-//  3. useContrxt(범위)로 고유된 값 사용하기
-
-//라이브러리 사용해서 간단한 animation 추가해보기
-//yarn add react-transition-group 입력
-//animation 만들기:
-//  1. <CSSTransition></CSSTransition> 으로 애니메이션 필요한 곳 감싸기
-//  2. in, classNames, timeout 넣기
-//  3. class로 애니메이션 넣기
-//  4. 원할 때 스위치 켜기 / 끄기 (버튼 누르면 스위치 끔) (컴포넌트 로드/업데이트시 스위치 켬)
-
 let stockContext = React.createContext();
 
 function App() {
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,16 +7,6 @@ import "./Detail.scss";
 import { CSSTransition } from "react-transition-group";
 // useParams 는 Route상에서 받아와지는 id를 이 function에 전달해 주는 역할을 함
 
-//styled-component: css 를 미리 입혀놓은 컴포넌트
-//편리하게 보이긴 하지만 수십개의 div에서 수십개의 컴포넌트로 바뀌는것
-//(class가 겹치는 가능성은 배제시켜주긴 한다) *선택사항
-
-// 컴포넌트들은 다 life cycle이 존재한다
-// 개발자는 hook으로 컴포넌트의 인생 중간중간에 뭔가 명령을 줄 수 있다
-// ex) <Detail/> 퇴장 전에 이것좀 해주세요
-// 요즘은 useEffect 라는 것을 사용해 훅을 만든다
-//몇 초 후에 코드를 실행하는 javascript 함수는 setTimeout(function()=>{}, 00ms)
-
 let Box = styled.div`
   padding: 20px;
 `;
@@ -47,19 +37,12 @@ function Detail(props) {
     }
   }
 
-  //컴포넌트가 등장/ 업데이트시 실행됨
-  //(따라서 input 값을 받아서 페이지에 표시하고 있으면 계속 업데이트 되고 있기 때문에 useEffect가 계속 실행됨)
-  // 따라서 useEffect(()=>{},[here]) 에 적어두면 here이라는 state가 변경이 될때만 실행이 되라는 함수가 완성이 된다
-  // useEffect의 대괄호를 빈칸으로 두면 해당 component가 등장시 '한 번' 실행하고 끝나는 함수가 된다
   useEffect(() => {
     setAniAlert(true);
     let timer = setTimeout(() => {
       setAniAlert(false);
       // setAlert(false);
     }, 2000);
-    // 컴포넌트가 사라질 때 코드를 실행시킬수도 있다
-    // return function 어쩌구() {실행할코드 ~~~}
-    // setTimeout을 사용할 때는 타이머 해제 스킬을 사용해야 나중에 버그가 안난다
     return () => {
       clearTimeout(timer);
     };
