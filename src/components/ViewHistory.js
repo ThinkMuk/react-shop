64ff188986d226e3b5e08b64b03d571ffc5a35fb
ThinkMuk <b2000kang@gmail.com>
2022-03-12 19:57:40 +0900

    2022-03-12 todo

--- src/components/ViewHistory.js
+++ src/components/ViewHistory.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./ViewHistory.scss";
 
 function visitedArray() {
@@ -17,19 +17,31 @@ function ViewHistory({ getShoeDatas }) {
       <div className="title">
         <h3>Visited</h3>
       </div>
-      {visited.length <= 0 ? (
-        <ItemIsEmpty />
-      ) : (
-        <div className="table">
-          {visited.map(function (a, i) {
-            return (
-              <Button variant="secondary" size="sm" key={i}>
-                {getShoeDatas[a].title}
-              </Button>
-            );
-          })}
-        </div>
-      )}
+      <div className="row">
+        {visited.length <= 0 ? (
+          <ItemIsEmpty />
+        ) : (
+          <div className="table">
+            {visited.map(function (a, i) {
+              return (
+                <div className="center" key={a}>
+                  <Link className="list-text-design" to={`/detail/${a}`}>
+                    <img
+                      src={`../images/shoes${parseInt(a) + 1}.jpg`}
+                      width="100%"
+                    />
+                    <h4>{getShoeDatas[a].title}</h4>
+                    <p>
+                      {getShoeDatas[a].content} <br />
+                      {getShoeDatas[a].price} won
+                    </p>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
